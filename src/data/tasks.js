9e6de97034316b608b3d9d0d44ee2ca49a6dd342
CarlosVernal <carlos.vernal.navarrete@gmail.com>
2024-06-22 16:07:33 -0400

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primera tarea",
+    description: "Mi primera tarea por hacer",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Mi segunda tarea por hacer",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "Mi tercera tarea por hacer",
+  },
+  {
+    id: 3,
+    title: "Mi cuarta tarea",
+    description: "Mi cuarta tarea por hacer",
+  },
+];
